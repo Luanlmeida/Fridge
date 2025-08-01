b2773caa3ce663a0865c7e7c08edfc282cf91538
Luanlmeida <luanlmeidaa7@gmail.com>
2025-08-01 14:47:47 -0300

    Update Food.java

--- src/main/java/dev/java10x/Fridge/model/Food.java
+++ src/main/java/dev/java10x/Fridge/model/Food.java
@@ -1,5 +1,61 @@
 package dev.java10x.Fridge.model;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = ("food_table"))
 public class Food {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int quantity;
+    private LocalDate expirationDate;
+
+    public Food(Long id, String name, int quantity, LocalDate expirationDate) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.expirationDate = expirationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(LocalDate expirationDate) {
+        this.expirationDate = expirationDate;
+    }
 
 }
